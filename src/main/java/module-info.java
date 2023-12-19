e27562e445775c072e43985ccfbf6cba56cfb69a
Hyun Suk Noh <hsnoh@philstar.biz>
2023-12-19 17:06:31 +0800

    Update versions
    - properly modularize
    - update gradle plugin versions

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.philstar.app.RaffleStar {
+    exports com.philstar.app.rafflestar;
+
+    requires javafx.controls;
+    requires javafx.graphics;
+//    requires javafx.css;
+    requires java.logging;
+}
